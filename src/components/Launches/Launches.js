6914f6a1905141813bdcc3898b8003edce6a632b
Ihor-Chupryna <igchup1985@gmail.com>
2024-02-06 20:09:38 +0200

    homework 2 SpaceX

--- src/components/Launches/Launches.js
+++ src/components/Launches/Launches.js
@@ -7,14 +7,21 @@ const Launches = () => {
     const [launches, setLaunches] = useState([])
 
     useEffect(() => {
-        launchesService.getAll().then(({data}) => setLaunches(data))
+        launchesService.getAll().then(({data}) => {
+            let filteredArray = data.filter(value => value.launch_year !== '2020')
+            setLaunches(filteredArray)
+        })
     }, []);
 
     return (
-        <div style={{background:'grey'}}>
-            <h1 style={{padding:'15px', textAlign:'center', color:'black'}}>SpaceX</h1>
-            <div style={{display:"flex", flexWrap:"wrap", justifyContent:"space-around", rowGap:"10px"}}>{launches.filter(value => value.launch_year < 2020)
-                .map(value => <Launch key={value.flight_number} launch={value}/>)}</div>
+        <div style={{background: 'grey'}}>
+            <h1 style={{padding: '15px', textAlign: 'center', color: 'black'}}>SpaceX</h1>
+            <div style={{
+                display: "flex",
+                flexWrap: "wrap",
+                justifyContent: "space-around",
+                rowGap: "10px"
+            }}>{launches.map(value => <Launch key={value.flight_number} launch={value}/>)}</div>
         </div>
     );
 };
