1f197f0199a3e31a10832abff5eedd09d522c0bb
Ihor-Chupryna <igchup1985@gmail.com>
2024-02-16 00:09:17 +0200

    homework5 routes loader

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { RouterProvider } from "react-router-dom";
 import './index.css';
 import { router } from "./router";
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
    <RouterProvider router={router}/>
